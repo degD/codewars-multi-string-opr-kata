cb174dc2d1ee62d0581d69b67ec2033d4de72f0c
degD <108623049+degD@users.noreply.github.com>
2023-09-16 12:42:12 +0300

    Added helper functions

--- main.sh
+++ main.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+# First parameter is a string. Echo this string in inverted case.
+function inverted_case {
+    str="$1"
+    echo "${str~~}"
+}
+
+# Reverse the given string.
+function reverse_string {
+    str="$1"
+    echo "$str" | rev 
+}
